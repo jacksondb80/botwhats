80aa6f58a8aac803cd69ad3807d2496802cd8423
Jackson Barcellos <jacksondb80@gmail.com>
2021-02-17 19:47:43 -0300

    adicionando venom

--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,7 @@
 "use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _express = require('express'); var _express2 = _interopRequireDefault(_express);
 var _routes = require('./routes'); var _routes2 = _interopRequireDefault(_routes);
 var _cors = require('cors'); var _cors2 = _interopRequireDefault(_cors);
+const venom = require('venom-bot');
 
 class App {
     constructor() {
@@ -8,6 +9,7 @@ class App {
 
         this.middlewares();
         this.routes();
+        this.cria();
     }
 
     middlewares() {
@@ -18,6 +20,51 @@ class App {
     routes() {
         this.server.use(_routes2.default);
     }
+
+    cria(){
+        venom
+            .create()
+            .then((client) => {
+
+            let time = 0;
+            client.onStreamChange((state) => {
+                console.log('Connection status: ', state);
+                clearTimeout(time);
+                if(state === 'CONNECTED'){
+                start(client);
+                }
+            //  DISCONNECTED when the mobile device is disconnected
+                if (state === 'DISCONNECTED' || state === 'SYNCING') {
+                time = setTimeout(() => {
+                    client.close();
+                // process.exit(); //optional function if you work with only one session
+                }, 80000);
+                }
+
+            })
+            .catch((erro) => {
+                console.log('There was an error in the bot',erro);
+            });
+        });
+    }
+
+    async start(client) {
+        let inchat = await client.isInsideChat(); //wait until the page is in whatsapp chat
+        if (inchat) {
+          client.onMessage((message) => {
+            if (message.body === 'Hi' && message.isGroupMsg === false) {
+              client
+                .sendText(message.from, 'Welcome Venom 🕷')
+                .then((result) => {
+                  console.log('Result: ', result); //return object success
+                })
+                .catch((erro) => {
+                  console.error('Error when sending: ', erro); //return object error
+                });
+            }
+          });
+        }
+      }
 }
 
 exports. default = new App().server;
--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import routes from './routes';
 import cors from 'cors';
+const venom = require('venom-bot');
 
 class App {
     constructor() {
@@ -8,6 +9,7 @@ class App {
 
         this.middlewares();
         this.routes();
+        this.cria();
     }
 
     middlewares() {
@@ -18,6 +20,51 @@ class App {
     routes() {
         this.server.use(routes);
     }
+
+    cria(){
+        venom
+            .create()
+            .then((client) => {
+
+            let time = 0;
+            client.onStreamChange((state) => {
+                console.log('Connection status: ', state);
+                clearTimeout(time);
+                if(state === 'CONNECTED'){
+                start(client);
+                }
+            //  DISCONNECTED when the mobile device is disconnected
+                if (state === 'DISCONNECTED' || state === 'SYNCING') {
+                time = setTimeout(() => {
+                    client.close();
+                // process.exit(); //optional function if you work with only one session
+                }, 80000);
+                }
+
+            })
+            .catch((erro) => {
+                console.log('There was an error in the bot',erro);
+            });
+        });
+    }
+
+    async start(client) {
+        let inchat = await client.isInsideChat(); //wait until the page is in whatsapp chat
+        if (inchat) {
+          client.onMessage((message) => {
+            if (message.body === 'Hi' && message.isGroupMsg === false) {
+              client
+                .sendText(message.from, 'Welcome Venom 🕷')
+                .then((result) => {
+                  console.log('Result: ', result); //return object success
+                })
+                .catch((erro) => {
+                  console.error('Error when sending: ', erro); //return object error
+                });
+            }
+          });
+        }
+      }
 }
 
 export default new App().server;
